6df6d8da516f46158d3f05e2ea5b3b81aed3053b
Indira Diana Puspasari <indiradiana239@gmail.com>
2024-06-07 14:29:10 +0700

    add styling for components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,29 @@
 import img_car from "../assets/img/img_car.png";
 import { Button } from "@mui/base";
 import Navbar from "./Navbar";
+import styles from "../components/styles/Header.module.css";
 
 export default function Header() {
   return (
     <>
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        <h1>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
-        <h3>
-          Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas
-          terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu
-          untuk sewa mobil selama 24 jam.
-        </h3>
-      </div>
-      <div>
-        <img src={img_car} alt="car image" />
-      </div>
-      <div>
-        <Button>Mulai Sewa Mobil</Button>
+      <div className={styles.container}>
+        <div>
+          <Navbar />
+        </div>
+        <div className={styles.text}>
+          <h1>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
+          <h3>
+            Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas
+            terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu
+            untuk sewa mobil selama 24 jam.
+          </h3>
+        </div>
+        <div className={styles.button}>
+          <Button>Mulai Sewa Mobil</Button>
+        </div>
+        <div className={styles.car}>
+          <img src={img_car} alt="car image" />
+        </div>
       </div>
     </>
   );
--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -1,35 +1,59 @@
 import img_service from "../assets/img/img_service.png";
 import list from "../assets/img/list.png";
+import styles from "../components/styles/OurServices.module.css";
+
+const dataService = [
+  {
+    image: list,
+    description: "Sewa Mobil Dengan Supir di Bali 12 Jam",
+  },
+  {
+    image: list,
+    description: "Sewa Mobil Lepas Kunci di Bali 24 Jam",
+  },
+  {
+    image: list,
+    description: "Sewa Mobil Jangka Panjang Bulanan",
+  },
+  {
+    image: list,
+    description: "Gratis Antar - Jemput Mobil di Bandara",
+  },
+  {
+    image: list,
+    description: "Layanan Airport Transfer / Drop In Out",
+  },
+];
 
 export default function OurServices() {
   return (
     <>
-      <div>
-        <img src={img_service} alt="service image" />
-      </div>
-      <div>
-        <ul>
-          <li>
-            <img src={list} alt="" />
-            <p>Sewa Mobil Dengan Supir di Bali 12 Jam</p>
-          </li>
-          <li>
-            <img src={list} alt="" />
-            <p>Sewa Mobil Lepas Kunci di Bali 24 Jam</p>
-          </li>
-          <li>
-            <img src={list} alt="" />
-            <p>Sewa Mobil Jangka Panjang Bulanan</p>
-          </li>
-          <li>
-            <img src={list} alt="" />
-            <p>Gratis Antar-Jemput Mobil di Bandara</p>
-          </li>
-          <li>
-            <img src={list} alt="" />
-            <p>Layanan Airport Transfer / Drop In Out</p>
-          </li>
-        </ul>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <img className={styles.image} src={img_service} alt="service image" />
+        </div>
+        <div className={styles.services}>
+          <h2>Best Car Rental for any kind of trip in (lokasimu)</h2>
+          <p>
+            Sewa mobil di (Lokasimu) bersama Binar Car Rental jaminan harga
+            lebih murah dibandingkan yang lain, kondisi mobil baru, serta
+            kualitas pelayanan terbaik untuk perjalanan wisata, bisnis, wedding,
+            meeting, dll.
+          </p>
+
+          <div>
+            {dataService.map((item, index) => (
+              <div className={styles.list} key={index}>
+                <img
+                  className={styles.check}
+                  src={item.image}
+                  alt={`gambar ke ${index}`}
+                />
+                <p>{item.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
